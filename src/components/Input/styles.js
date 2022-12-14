ed7e71f6cd1a8329e3917859177ebee9ab55b7c3
lucasrbl <lucasrblemos@gmail.com>
2022-12-14 19:11:32 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div `
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        font-family: 'Roboto';
+        border: none;
+        font-size: 24px;
+        color: #FFFFFF;
+        padding: 0 10px;
+    }
+`
